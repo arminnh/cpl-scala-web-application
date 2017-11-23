b54bf1d41f5d646755fa55349a1adc38b15ea8c6
Armin <halilovic.armin@hotmail.com>
2017-11-23 12:20:22 +0100

    Fixed and added more test code so that the project compiles and runs.

--- src/js/src/main/scala/be/kuleuven/proman/ProManApp.scala
+++ src/js/src/main/scala/be/kuleuven/proman/ProManApp.scala
@@ -1,3 +1,11 @@
+// package be.kuleuven.proman
+
+import scala.scalajs.js
+import org.scalajs.dom
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+
+import dom.ext.Ajax
+
 object ProManApp {
 
   print("hello wtf")
@@ -5,5 +13,36 @@ object ProManApp {
   def main(args: Array[String]): Unit = {
     print("wtffff")
 
+    var xhr = new dom.XMLHttpRequest()
+    xhr.open("GET", "/service/animals")
+    xhr.onload = { (e: dom.Event) =>
+        if (xhr.status == 200) {
+            val container = dom.document.querySelector("#animals-container")
+
+            // val animals: Either[io.circe.Error, List[Animal]] = decode[List[Animal]](xhr.responseText)
+            //
+            // animals match {
+            //     case Left(error) => dom.window.alert(error.getMessage)
+            //     case Right(list) => list.foreach( animal => {
+            //         val li = dom.document.createElement("li")
+            //         li.textContent = animal.breed + ", " + animal.name
+            //         container.appendChild(li)
+            //     })
+            // }
+            //
+            // // animals.right = List[List[Animal]] ???
+            // animals.right.foreach(
+            //     wtf => wtf.foreach(
+            //         animal => {
+            //             val li = dom.document.createElement("li")
+            //             li.textContent = animal.breed + ", " + animal.name
+            //             container.appendChild(li)
+            //         }
+            //     )
+            // )
+        }
+
+    }
+    xhr.send()
   }
 }
--- src/jvm/src/main/scala/be/kuleuven/proman/ProManApp.scala
+++ src/jvm/src/main/scala/be/kuleuven/proman/ProManApp.scala
@@ -1,17 +1,37 @@
+// package be.kuleuven.proman
+
+// import collection.mutable.Seq
+import scala.io.StdIn
+
+import java.io.File
 import java.time.LocalDateTime
 
-import collection.mutable.Seq
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl._
+import org.http4s.server.blaze.BlazeBuilder
+import fs2.Task
+import fs2.interop.cats._
+import scalatags.Text._
+import scalatags.Text.all._
+import scalatags.Text.tags2.title
+import io.circe.generic.auto._
+import io.circe.syntax._
+
 
 object ProManApp extends App {
   val test_todo = new TODOEntry("Finish project", LocalDateTime.now())
-  val projects: Seq[TODOProject] = List(new TODOProject("SCALA PROJECT", List(test_todo)))
+  val projects: Seq[TODOProject] = List(
+      new TODOProject("SCALA PROJECT", List(test_todo))
+  )
 
   print(test_todo)
   projects.foreach(p => print(p.toString))
 
   val service: HttpService = HttpService {
-    case GET -> Root / "hello" =>
-      homepage
+    case GET -> Root / "hello" => homepage
+
+    case GET -> Root / "js" => javascriptProgramResponse
 
     case GET -> Root / name => Ok(s"Hello, $name")
   }
@@ -25,7 +45,8 @@ object ProManApp extends App {
       body(
         h1("ANIMALS!!!!"),
         ul(
-          animals.map( a => li(a.breed + ", " + a.name) )
+            li("aaaaa"),
+            li("aaaaa")
         )
         //div(
         //  h1("Breed, Name")
@@ -36,6 +57,20 @@ object ProManApp extends App {
   }.withType(MediaType.`text/html`)
 
   lazy val projectsJSON: Task[Response] = Ok {
-    projects.asJson
+    // projects.asJson
+    List(1, 2, 3).asJson
   }
+
+  // compilation result of the Scala.js code.
+  lazy val javascriptProgramResponse: Task[Response] =
+    StaticFile
+      .fromFile(new File("./js/target/scala-2.11/js-fastopt.js"))
+      .getOrElseF(NotFound())
+
+  // Standard server that binds to localhost:8080/
+  val server = BlazeBuilder.bindHttp(8080, "localhost").mountService(service, "/").run
+
+  // Block on reading a line, makes it easy to stop in your terminal (hit enter).
+  StdIn.readLine()
+  server.shutdownNow()
 }
